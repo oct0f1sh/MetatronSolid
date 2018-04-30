dd950d0279a4e4d2eaa02dbb626d16a24f7685b4
Duncan <buzzbuzz.macdonald@gmail.com>
2018-04-30 09:30:57 -0700

    Initial Commit

---
+++ animations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  animations
+//
+//  Created by Duncan MacDonald on 4/30/18.
+//  Copyright © 2018 Duncan MacDonald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
